b19b527810df06b435b0c1d864cd4888a00abb90
Jason Mills <jason.mills@integratelecom.com>
2016-12-10 15:46:06 -0800

    Correct schema definition from auto generator
    SQLite has some strange ideas about auto incrementing primary keys. Auto incrementing is apparently implicit when using an Integer type with primary key. Initial tests failed due to post_id’s not getting incremented from the in-memory deployed DDL.

--- blog/model/store.py
+++ blog/model/store.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 # generated using: sqlacodegen sqlite:///blog.db >blog/model/store.py
-from sqlalchemy import Column, Numeric
-from sqlalchemy.sql.sqltypes import NullType
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -12,6 +11,6 @@ metadata = Base.metadata
 class Post(Base):
     __tablename__ = 'posts'
 
-    post_id = Column(NullType, primary_key=True)
-    title = Column(Numeric)
-    body = Column(Numeric)
+    post_id = Column(Integer, primary_key=True)
+    title = Column(String)
+    body = Column(String)
