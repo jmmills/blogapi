fbeada4cbb4ed342c1000592d48d90fbf9cbe711
Jason Mills <jason.mills@integratelecom.com>
2016-12-11 11:35:35 -0800

    Updates db.py: adds drop command
    Also, uses python3 string format rather than v2 style

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-A quick script to allow us to deploy our Blog schema easily
+A quick script to allow us to drop or deploy our Blog schema easily
 using SQLAlchemy.
 
 In the case of the future we need to write up any migrations for schema changes
@@ -18,12 +18,21 @@ engine = None
 @click.group()
 @click.option('--uri', help="SQLAlchemy URI to deploy schema to")
 def cli(uri):
-    click.echo('Using "%s" connection URI'.format(uri))
-    PostMeta.create_all(engine)
+    click.echo('Using "{}" connection URI'.format(uri))
+    global engine
+    engine = create_engine(uri)
 
 @cli.command()
 def deploy():
+    """Deploys blog schema"""
+    click.echo('Creating schema')
     PostMeta.create_all(engine)
+
+@cli.command()
+def drop():
+    """Drops a blog schema"""
+    click.echo('Dropping schema')
+    PostMeta.drop_all(engine)
 
 
 if __name__ == '__main__':
